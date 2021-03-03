63aa668bd1c561adfc91c34abcdfa9421c259824
Tom Hummel <tphummel@gmail.com>
2021-03-03 15:24:53 -0800

    add install steps for caddy and node.sj

--- vagrant-setup.sh
+++ vagrant-setup.sh
@@ -16,11 +16,22 @@ main(){
 }
 
 install_caddy(){
+  # https://caddyserver.com/docs/install#fedora-redhat-centos
   echo "installing caddy"
+  sudo dnf --assumeyes install 'dnf-command(copr)'
+  sudo dnf --assumeyes copr enable @caddy/caddy
+  sudo dnf --assumeyes install caddy
+  which caddy
+  caddy version
 }
 
 install_nodejs(){
+  # https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-centos-8
   echo "installing node.js"
+  sudo dnf --assumeyes module enable nodejs:14
+  sudo dnf --assumeyes install nodejs
+  which node
+  node --version
 }
 
 install_sqlite3(){
