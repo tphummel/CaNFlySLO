df754dc2623b7a9a2154d307703f0fc6dca3d113
Tom Hummel <tphummel@gmail.com>
2021-03-03 14:50:10 -0800

    a start

---
+++ Vagrantfile
@@ -0,0 +1,22 @@
+# https://app.vagrantup.com/centos/boxes/8
+Vagrant.configure("2") do |config|
+  config.vm.box = "centos/8"
+
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 1024
+    v.cpus = 2
+    v.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+    v.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
+  end
+
+  config.ssh.forward_agent = true
+  config.vm.synced_folder ".", "/project", type: "nfs"
+
+  config.vm.define "can-fly-slo" do |node|
+    node.vm.network "forwarded_port", guest: 9090, host: 9090
+    node.vm.network "forwarded_port", guest: 8080, host: 8080
+    node.vm.network "forwarded_port", guest: 2019, host: 2019
+    node.vm.network "private_network", ip: "192.168.13.4"
+    node.vm.hostname = "cfs.dev"
+  end
+end
---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+const env = {
+  database: 'testdb',
+  username: 'root',
+  password: '12345',
+  host: 'localhost',
+  dialect: 'sqlite',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+}
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(env.database, env.username, env.password, {
+  host: env.host,
+  dialect: env.dialect,
+  operatorsAliases: false,
+
+  pool: {
+    max: env.max,
+    min: env.pool.min,
+    acquire: env.pool.acquire,
+    idle: env.pool.idle
+  }
+})
+
+sequelize.sync({ force: true }).then(() => {
+  console.log('Drop and Resync with { force: true }')
+})
+
+const Customer = sequelize.define('customer', {
+  firstname: {
+    type: Sequelize.STRING
+  },
+  lastname: {
+    type: Sequelize.STRING
+  },
+  age: {
+    type: Sequelize.INTEGER
+  }
+})
+
+app.post('/api/customers', (req, res) => {
+  Customer.create({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    age: req.body.age
+  }).then(customer => {
+    res.send(customer)
+  })
+})
+
+app.get('/api/customers', (req, res) => {
+  Customer.findAll().then(customers => {
+    res.send(customers)
+  })
+})
+
+app.get('/api/customers/:customerId', (req, res) => {
+  Customer.findById(req.params.customerId).then(customer => {
+    res.send(customer)
+  })
+})
+
+app.put('/api/customers/:customerId', (req, res) => {
+  const id = req.params.customerId
+  Customer.update({ firstname: req.body.firstname, lastname: req.body.lastname, age: req.body.age },
+    { where: { id: req.params.customerId } }
+  ).then(() => {
+    res.status(200).send('updated successfully a customer with id = ' + id)
+  })
+})
+
+app.delete('/api/customers/:customerId', (req, res) => {
+  const id = req.params.customerId
+  Customer.destroy({
+    where: { id: id }
+  }).then(() => {
+    res.status(200).send('deleted successfully a customer with id = ' + id)
+  })
+})
+
+const server = app.listen(8081, function () {
+  const host = server.address().address
+  const port = server.address().port
+
+  console.log('App listening at http://%s:%s', host, port)
+})
