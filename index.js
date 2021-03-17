a241a354d7b084314897763294767644cabcd9a0
Tom Hummel <tphummel@gmail.com>
2021-03-17 14:36:01 -0700

    bring back express 4

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const createServer = require('http-hash-server')
-const formBody = require('body/form')
+const formBody = require('body-parser').urlencoded({ extended: true })
 
 const waterfall = require('run-waterfall')
 
@@ -26,32 +26,12 @@ const user = {
 
 const userRoutes = {
   post: (req, res, opts) => {
-    waterfall([
-      (cb) => {
-        return formBody(req, cb)
-      },
-      (body, cb) => {
-        console.log(body)
-        // form data not actually decoding
-        /*
-        {
-          '--------------------------73a4d03446b58264\r\nContent-Disposition: form-data; name': '"email"\r\n' +
-            '\r\n' +
-            'tphummel@gmail.com\r\n' +
-            '--------------------------73a4d03446b58264--\r\n'
-        }
-
-        */
-        // return user.create(body.email, cb)
-        return user.create('tphummel@gmail.com', cb)
-      }
-    ], (err) => {
+    user.create(req.body.email, (err) => {
       if (err) {
-        res.responseCode = 500
+        res.status(500).end(err)
       } else {
-        res.responseCode = 201
+        res.status(201).end()
       }
-      res.end()
     })
   }
 }
@@ -59,42 +39,21 @@ const userRoutes = {
 const host = '127.0.0.1'
 const port = 8081
 
-const server = createServer({
-  hostname: host,
-  port: port,
-  services: {
-    user: {
-      route: '/',
-      methods: {
-        create: {
-          httpMethod: 'POST',
-          route: '/user',
-          handler: userRoutes.post
-        }
-      }
-    },
-    metrics: {
-      route: '/',
-      methods: {
-        get: {
-          httpMethod: 'GET',
-          route: '/metrics',
-          handler: async (req, res, opts) => {
-            try {
-              res.setHeader('Content-Type', p8s.register.contentType)
-              res.statusCode = 200
-              res.end(await p8s.register.metrics(), 'utf8')
-            } catch (ex) {
-              res.statusCode = 500
-              res.end(ex)
-            }
-          }
-        }
-      }
-    }
+const express = require('express')
+const app = express()
+
+app.post('/users', formBody, userRoutes.post)
+app.get('/metrics', async (req, res, opts) => {
+  try {
+    res.setHeader('Content-Type', p8s.register.contentType)
+    res.statusCode = 200
+    res.end(await p8s.register.metrics(), 'utf8')
+  } catch (ex) {
+    res.statusCode = 500
+    res.end(ex)
   }
 })
 
-server.listen((err) => {
-  console.log('App listening at http://%s:%s', host, port)
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
