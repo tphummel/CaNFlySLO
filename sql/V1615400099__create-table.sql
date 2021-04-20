82fb240e76fb9b299986356136292f0cdabc52da
Tom Hummel <tphummel@gmail.com>
2021-04-19 22:13:20 -0700

    update db schema for nertz w/ groundwork for multi-user / rbac

--- sql/V1615400099__create-table.sql
+++ sql/V1615400099__create-table.sql
@@ -1,19 +1,59 @@
-CREATE TABLE user (
+CREATE TABLE tenant (
   id INTEGER PRIMARY KEY,
-  email TEXT NOT NULL,
   created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TEXT DEFAULT NULL
 );
 
-CREATE INDEX user_email ON user(email);
+CREATE TABLE customer (
+  id INTEGER PRIMARY KEY,
+  tenant_id INTEGER NOT NULL,
+  email TEXT NOT NULL,
+  name TEXT DEFAULT NULL,
+  tokens_invalid_before TEXT DEFAULT NULL,
+  created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TEXT DEFAULT NULL,
+  last_login_at TEXT DEFAULT NULL,
+  FOREIGN KEY(tenant_id) REFERENCES tenant(id)
+);
+
+CREATE INDEX customer_email ON customer(email);
 
 CREATE TABLE player (
   id INTEGER PRIMARY KEY,
+  tenant_id INTEGER NOT NULL,
   handle TEXT NOT NULL,
   fullname TEXT DEFAULT NULL,
   avatar BLOB DEFAULT NULL,
-  created_by INTEGER DEFAULT NULL,
+  created_by INTEGER NOT NULL,
   created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TEXT DEFAULT NULL,
+  FOREIGN KEY(tenant_id) REFERENCES tenant(id),
+  FOREIGN KEY(created_by) REFERENCES customer(id)
+);
+
+CREATE TABLE venue (
+  id INTEGER PRIMARY KEY,
+  tenant_id INTEGER NOT NULL,
+  handle TEXT NOT NULL,
+  name TEXT NOT NULL,
+  avatar BLOB DEFAULT NULL,
+  created_by INTEGER NOT NULL,
+  created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TEXT DEFAULT NULL,
+  FOREIGN KEY(tenant_id) REFERENCES tenant(id),
+  FOREIGN KEY(created_by) REFERENCES user(id)
+);
+
+CREATE TABLE ruleset (
+  id INTEGER PRIMARY KEY,
+  tenant_id INTEGER NOT NULL,
+  handle TEXT NOT NULL,
+  name TEXT NOT NULL,
+  pile INTEGER NOT NULL,
+  bonus INTEGER NOT NULL,
+  created_by INTEGER NOT NULL,
+  created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TEXT DEFAULT NULL,
+  FOREIGN KEY(tenant_id) REFERENCES tenant(id),
   FOREIGN KEY(created_by) REFERENCES user(id)
 );
