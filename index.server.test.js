4d0e31bbc6c17f22b8639392486b78307784dd1b
Tom Hummel <tphummel@gmail.com>
2021-04-27 21:38:54 -0700

    add server test for GET /login
    before:
    
    ```
    Statements   : 55.63% ( 79/142 )
    Branches     : 22.92% ( 11/48 )
    Functions    : 45.24% ( 19/42 )
    Lines        : 59.4% ( 79/133 )
    ```
    
    after:
    
    ```
    Statements   : 56.34% ( 80/142 )
    Branches     : 25% ( 12/48 )
    Functions    : 47.62% ( 20/42 )
    Lines        : 60.15% ( 80/133 )
    ```

--- index.server.test.js
+++ index.server.test.js
@@ -2,6 +2,7 @@
 
 const url = require('url')
 const tap = require('tap')
+const { v4: uuidv4 } = require('uuid')
 const get = require('simple-get')
 const waterfall = require('run-waterfall')
 
@@ -29,6 +30,43 @@ tap.test('GET /', function (t) {
             protocol: 'http',
             hostname: 'localhost',
             pathname: '/',
+            port: port
+          })
+        }, (err, res) => {
+          t.ifErr(err)
+          t.equal(res.statusCode, 200)
+
+          server.close(cb)
+        })
+      })
+    }
+  ], (err) => {
+    t.ifErr(err)
+    t.end()
+  })
+})
+
+tap.test('GET /login', function (t) {
+  process.env.LOGIN_JWT_SECRET = uuidv4()
+  process.env.SESSION_JWT_SECRET = uuidv4()
+
+  const lib = require('./index.js')
+  const dbfile = dbTestUtils.getFilename()
+  process.env.DBFILE = dbfile
+
+  waterfall([
+    (cb) => { dbTestUtils.setup(dbfile, cb) },
+    // gotcha: getPort doesn't use standard errback pattern, for reasons.
+    (stdout, stderr, cb) => { getPort((port) => { return cb(null, port) }) },
+    (port, cb) => {
+      const server = lib.start(lib.app, port, (err) => {
+        t.ifErr(err)
+
+        get({
+          url: url.format({
+            protocol: 'http',
+            hostname: 'localhost',
+            pathname: '/login',
             port: port
           })
         }, (err, res) => {
