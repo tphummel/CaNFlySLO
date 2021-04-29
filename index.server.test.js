099e69fa0c2066db21581a22cbb2fe97add64083
Tom Hummel <tohu@hey.com>
2021-04-28 20:47:26 -0700

    add failed GET /home test

--- index.server.test.js
+++ index.server.test.js
@@ -202,3 +202,40 @@ tap.test('POST /login with malformed email', function (t) {
     t.end()
   })
 })
+
+tap.test('GET /home without a session should get 401', function (t) {
+  process.env.LOGIN_JWT_SECRET = uuidv4()
+  process.env.SESSION_JWT_SECRET = uuidv4()
+
+  const lib = require('./index.js')
+  const dbfile = dbTestUtils.getFilename()
+  process.env.DBFILE = dbfile
+
+  waterfall([
+    (cb) => { dbTestUtils.setup(dbfile, cb) },
+    // gotcha: getPort doesn't use standard errback pattern, for reasons.
+    (stdout, stderr, cb) => { getPort((port) => { return cb(null, port) }) },
+    (port, cb) => {
+      const server = lib.start(lib.app, port, (err) => {
+        t.ifErr(err)
+
+        get({
+          url: url.format({
+            protocol: 'http',
+            hostname: 'localhost',
+            pathname: '/home',
+            port: port
+          })
+        }, (err, res) => {
+          t.ifErr(err)
+          t.equal(res.statusCode, 401)
+
+          server.close(cb)
+        })
+      })
+    }
+  ], (err) => {
+    t.ifErr(err)
+    t.end()
+  })
+})
