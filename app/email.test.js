36768c81ba4a789974ca7c4d8fb8fe521051d214
Tom Hummel <tphummel@gmail.com>
2021-04-28 19:48:37 -0700

    explain how we're managing email send mode

--- app/email.test.js
+++ app/email.test.js
@@ -5,6 +5,7 @@ tap.test('send a stdout login email', function (t) {
   const email = 'tohu@hey.com'
   const token = 'a-jwt-token'
 
+  // if this env var is falsy, it will go into stdout mode
   process.env.EMAIL_KEY = ''
 
   lib.sendLoginEmail({ customer: { email }, token }, (err) => {
