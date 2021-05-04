8787a33ccef2333508bd602ec13aa42aefed981f
Tom Hummel <tohu@hey.com>
2021-05-03 21:37:40 -0700

    add pragma queries to db startup
    still isn't perfect as we don't wait for the full startup sequence before returning the server start. there is some sync/async stuff to workout if i really care about that. 
    
    also, I can prove that the per test database files are being created. and that app.sqlite3 dev file is not being used. 
    
    before test run:
    ```
    [vagrant@cfs vagrant]$ ls -l app.sqlite3 
    -rw-rw-r--. 1 501 games 40960 May  4  2021 app.sqlite3
    [vagrant@cfs vagrant]$ ls -l /tmp/ | wc -l
    327
    ```
    
    test run:
    ```
    npm t
    ```
    
    after test run: 
    ```
    [vagrant@cfs vagrant]$ ls -l app.sqlite3 
    -rw-rw-r--. 1 501 games 40960 May  4  2021 app.sqlite3
    [vagrant@cfs vagrant]$ ls -l /tmp/ | wc -l
    334
    ```
    
    7 new sqlite files in /tmp after the test run. and no size added to app.sqlite3. Feeling pretty confident about my harness now.

--- app/db.js
+++ app/db.js
@@ -4,6 +4,35 @@ const sql = require('sqlite3')
 const path = require('path')
 
 const dbFile = process.env.DBFILE || path.join(__dirname, '..', 'app.sqlite3')
-const db = new sql.Database(dbFile)
+
+// instantiation automatically opens the db. there is no separate connect() call
+// https://github.com/mapbox/node-sqlite3/wiki/API#new-sqlite3databasefilename-mode-callback
+
+const db = new sql.Database(dbFile, sql.OPEN_READWRITE | sql.OPEN_CREATE, (err) => {
+  if (err) {
+    console.error(`error opening the sqlite3 database: ${err}`)
+    process.exit(1)
+  } else {
+    console.log('sqlite3 database opened successfully')
+
+    // https://litestream.io/tips/
+    // https://news.ycombinator.com/item?id=26103776
+    // 64MB cache file size. yes -64000 is correct. negative.
+    const pragmaQueries = `
+  PRAGMA journal_mode=WAL;
+  PRAGMA synchronous = NORMAL;
+  PRAGMA busy_timeout = 5000;
+  PRAGMA cache_size = -64000;
+  `
+
+    db.run(pragmaQueries, [], (err) => {
+      if (err) {
+        console.error(`error running sqlite3 pragma queries at startup: ${err}`)
+        process.exit(1)
+      }
+      console.log('sqlite3 startup pragma queries successful')
+    })
+  }
+})
 
 module.exports = db
