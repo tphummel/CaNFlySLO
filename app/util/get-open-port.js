0888a7c225e23a000991a367a66b2e7d8c1c47bb
Tom Hummel <tohu@hey.com>
2022-04-18 22:08:10 -0700

    just generate a random port number, for goodness sake

--- app/util/get-open-port.js
+++ app/util/get-open-port.js
@@ -1,26 +1,9 @@
 'use strict'
-// ignoring this file in standard
 
-const net = require('net')
-
-let portrange = 45032
-
-// https://gist.github.com/mikeal/1840641
-// doesn't use standard errback pattern for rescursive convenience
 function getPort (cb) {
-  const port = portrange
-  portrange += 1
-
-  const server = net.createServer()
-  server.listen(port, function (err) {
-    server.once('close', function () {
-      cb(port)
-    })
-    server.close()
-  })
-  server.on('error', function (err) {
-    getPort(cb)
-  })
+  const randomPort = Math.floor(Math.random() * (65535 - 49152) + 49152)
+  console.log('assigning', randomPort)
+  return setImmediate(cb.bind(null, randomPort))
 }
 
 module.exports = getPort
